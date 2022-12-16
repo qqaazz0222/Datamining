e0a2c2c39c690bc74e68f5c2a2a45a33646c08f7
qqaazz0222 <qqaazz0222@naver.com>
2022-12-17 04:54:48 +0900

    data division 6:4

--- DM_final_ucla.R
+++ DM_final_ucla.R
@@ -10,3 +10,13 @@ library(caret)
 ucla = read.csv('https://stats.idre.ucla.edu/stat/data/binary.csv')
 ucla$admit = factor(ucla$admit)
 str(ucla)
+
+rownum = nrow(ucla)
+index = 1:rownum
+# 트레이닝셋 : 테스트셋 = 6: 4
+trainingdata = sample(index, rownum*0.6)
+testdata = setdiff(index, trainingdata)
+ucla_training = ucla[trainingdata, ]
+ucla_test = ucla[testdata, ]
+nrow(ucla_training)
+nrow(ucla_test)
